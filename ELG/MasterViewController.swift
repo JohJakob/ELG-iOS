40e18364cf2c690aba2a8bc21f710459b22d0ad0
Johannes Jakob <johannes.jakob@elg-halle.de>
2016-07-21 14:41:47 +0200

    Change back indicator image + add 'Schulkreuz' as title view in main menu + rename assets
    Change back indicator image + add 'Schulkreuz' as title view in main
    menu + rename assets

--- ELG/MasterViewController.swift
+++ ELG/MasterViewController.swift
@@ -17,6 +17,16 @@ class MasterViewController: UITableViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     
+    // Set back indicator image
+    
+    navigationController?.navigationBar.backIndicatorImage = UIImage(named: "Back")
+    navigationController?.navigationBar.backIndicatorTransitionMaskImage = UIImage(named: "Back")
+    navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .Plain, target: nil, action: nil)
+    
+    // Set title view
+    
+    navigationItem.titleView = UIImageView.init(image: UIImage(named: "Schulkreuz"))
+    
     // Initialize user defaults
     
     if #available(iOS 8, *) {
