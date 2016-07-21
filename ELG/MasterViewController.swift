3d000f6275d18968cc55b28861c3b174e1f26bcc
Johannes Jakob <johannes.jakob@elg-halle.de>
2016-07-21 18:01:33 +0200

    Move navigation bar customization to function setUp in MasterViewController.swift

--- ELG/MasterViewController.swift
+++ ELG/MasterViewController.swift
@@ -17,16 +17,6 @@ class MasterViewController: UITableViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    // Set back indicator image
-    
-    navigationController?.navigationBar.backIndicatorImage = UIImage(named: "Back")
-    navigationController?.navigationBar.backIndicatorTransitionMaskImage = UIImage(named: "Back")
-    navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .Plain, target: nil, action: nil)
-    
-    // Set title view
-    
-    navigationItem.titleView = UIImageView.init(image: UIImage(named: "Schulkreuz"))
-    
     // Initialize user defaults
     
     if #available(iOS 8, *) {
@@ -60,6 +50,16 @@ class MasterViewController: UITableViewController {
   // Custom functions
   
   func setUp() {
+    // Set back indicator image
+    
+    navigationController?.navigationBar.backIndicatorImage = UIImage(named: "Back")
+    navigationController?.navigationBar.backIndicatorTransitionMaskImage = UIImage(named: "Back")
+    navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .Plain, target: nil, action: nil)
+    
+    // Set title view
+    
+    navigationItem.titleView = UIImageView.init(image: UIImage(named: "Schulkreuz"))
+    
     // Set empty schedules
     
     let emptySchedule = ["", "", "", "", "", "", "", "", "", ""]
