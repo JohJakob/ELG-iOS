19ac811b4768948e280c8afa85b7977f7b00c288
Johannes Jakob <johannes.jakob@elg-halle.de>
2016-08-13 09:35:16 +0200

    Add FoerdervereinViewController.swift

---
+++ ELG/FoerdervereinViewController.swift
@@ -0,0 +1,21 @@
+//
+//  FoerdervereinViewController.swift
+//  ELG
+//
+//  Created by Johannes Jakob on 13/08/2016
+//  ©2016 Elisabeth-Gymnasium Halle, Johannes Jakob
+//
+
+import UIKit
+
+class FoerdervereinViewController: UITableViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    
+    print("Memory Warning")
+  }
+}
