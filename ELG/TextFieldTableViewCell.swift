5174a2ff77e595a764e57aa165a7801255859324
Johannes Jakob <johannes.jakob@elg-halle.de>
2016-08-14 18:12:18 +0200

    Add TextFieldTableViewCell.xib and TextFieldTableViewCell.swift

---
+++ ELG/TextFieldTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TextFieldTableViewCell.swift
+//  ELG
+//
+//  Created by Johannes Jakob on 14.08.2016.
+//  Copyright © 2016 Elisabeth-Gymnasium Halle. All rights reserved.
+//
+
+import UIKit
+
+class TextFieldTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
