2275ec763e467dd0fd0473d1298657b196e9ff92
Johannes Jakob <johannes.jakob@elg-halle.de>
2016-08-14 14:39:43 +0200

    Add LoginViewController.swift

---
+++ ELG/LoginViewController.swift
@@ -0,0 +1,21 @@
+//
+//  LoginViewController.swift
+//  ELG
+//
+//  Created by Johannes Jakob on 14/08/2016
+//  ©2016 Elisabeth-Gymnasium Halle, Johannes Jakob
+//
+
+import UIKit
+
+class LoginViewController: UITableViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    
+    print("Memory Warning")
+  }
+}
