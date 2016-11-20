8278b6e5872ba3b2e00fa357144f330e6264b78d
Johannes Jakob <johannes.jakob@elg-halle.de>
2016-11-20 20:56:51 +0100

    Add teacher mode functionality to TeacherModeViewController.swift

--- ELG/TeacherModeViewController.swift
+++ ELG/TeacherModeViewController.swift
@@ -8,10 +8,152 @@
 
 import UIKit
 
-class TeacherModeViewController: UITableViewController {
+class TeacherModeViewController: UITableViewController, UITextFieldDelegate {
+  // Variables
+  
+  var defaults: NSUserDefaults!
+  var teacherMode = Bool()
+  var teacherToken = String()
+  var teacherModeSwitch = UISwitch()
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    
+    // Initialize user defaults
+    
+    if #available(iOS 8, *) {
+      defaults = NSUserDefaults.standardUserDefaults()
+    } else {
+      defaults = NSUserDefaults.init(suiteName: "group.com.hardykrause.elg")
+    }
+    
+    // Register custom table view cell
+    
+    tableView.registerNib(UINib(nibName: "TextFieldTableViewCell", bundle: nil), forCellReuseIdentifier: "TextFieldTableViewCell")
+    
+    // Initialize switch
+    
+    initSwitch()
+  }
+  
+  override func viewWillAppear(animated: Bool) {
+    super.viewWillAppear(animated)
+    
+    // Retrieve user defaults
+    
+    retrieveUserDefault()
+  }
+  
+  override func viewWillDisappear(animated: Bool) {
+    super.viewWillDisappear(animated)
+    
+    // Set user defaults
+    
+    setUserDefault()
+  }
+  
+  // Table view functions
+  
+  override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    return 1
+  }
+  
+  override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 2
+  }
+  
+  override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    if indexPath.row == 0 {
+      let cell = tableView.dequeueReusableCellWithIdentifier("TeacherModeTableViewCell", forIndexPath: indexPath)
+      
+      cell.textLabel!.text = "Lehrermodus"
+      cell.accessoryView = teacherModeSwitch
+      cell.selectionStyle = .None
+      
+      return cell
+    } else {
+      let cell = tableView.dequeueReusableCellWithIdentifier("TextFieldTableViewCell", forIndexPath: indexPath) as! TextFieldTableViewCell
+      
+      cell.textField.placeholder = "Kürzel"
+      cell.textField.text = teacherToken
+      
+      cell.textField.delegate = self
+      
+      return cell
+    }
+  }
+  
+  override func tableView(tableView: UITableView, titleForFooterInSection section: Int) -> String? {
+    return "Bei aktiviertem Lehrermodus werden in den eigenen Vertretungen nur Eintragungen mit dem Kürzel angezeigt."
+  }
+  
+  // Text field functions
+  
+  func textFieldDidEndEditing(textField: UITextField) {
+    // Set variable
+    
+    teacherToken = textField.text!
+    
+    // Resign first responder
+    
+    textField.resignFirstResponder()
+  }
+  
+  func textFieldShouldReturn(textField: UITextField) -> Bool {
+    // Resign first responder
+    
+    textField.resignFirstResponder()
+    
+    return true
+  }
+  
+  // Custom functions
+  
+  func initSwitch() {
+    // Retrieve user default
+    
+    teacherMode = defaults.boolForKey("teacherMode")
+    
+    // Initialize switch
+    
+    teacherModeSwitch = UISwitch.init(frame: CGRectZero)
+    
+    if teacherMode {
+      teacherModeSwitch.setOn(true, animated: false)
+    } else {
+      teacherModeSwitch.setOn(false, animated: false)
+    }
+    
+    teacherModeSwitch.addTarget(self, action: #selector(TeacherModeViewController.toggleTeacherMode), forControlEvents: .ValueChanged)
+    
+    teacherModeSwitch.onTintColor = UIColor(red: 0.498, green: 0.09, blue: 0.203, alpha: 1)
+  }
+  
+  func retrieveUserDefault() {
+    // Retrieve user default
+    
+    teacherToken = defaults.stringForKey("teacherToken")!
+  }
+  
+  func setUserDefault() {
+    // Set user default
+    
+    defaults.setBool(teacherMode, forKey: "teacherMode")
+    defaults.setObject(teacherToken, forKey: "teacherToken")
+    defaults.synchronize()
+  }
+  
+  func toggleTeacherMode() {
+    // Set user default
+    
+    if teacherModeSwitch.on {
+      teacherMode = true
+    } else {
+      teacherMode = false
+    }
+    
+    defaults.setBool(teacherMode, forKey: "teacherMode")
+    defaults.synchronize()
   }
   
   override func didReceiveMemoryWarning() {
