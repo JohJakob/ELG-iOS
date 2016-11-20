667fd41c20c9e93b9bf0b53e6ccf0198057f1f2e
Johannes Jakob <johannes.jakob@elg-halle.de>
2016-11-20 20:21:49 +0100

    Add TeacherModeViewController.swift

---
+++ ELG/TeacherModeViewController.swift
@@ -0,0 +1,22 @@
+//
+//  TeacherModeViewController.swift
+//  ELG
+//
+//  Created by Johannes Jakob on 20/11/2016
+//  © 2016 Elisabeth-Gymnasium Halle, Johannes Jakob
+//
+
+import UIKit
+
+class TeacherModeViewController: UITableViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    
+    print("Memory Warning")
+  }
+}
