7d14ab14b79f33645dde5817340cd63aa70b8e88
Johannes Jakob <johannes.jakob@elg-halle.de>
2016-11-27 14:00:24 +0100

    Add AboutWebViewController.swift

---
+++ ELG/AboutWebViewController.swift
@@ -0,0 +1,21 @@
+//
+//  AboutWebViewController.swift
+//  ELG
+//
+//  Created by Johannes Jakob on 27/11/2016
+//  © 2016 Elisabeth-Gymnasium Halle, Johannes Jakob
+//
+
+import UIKit
+
+class AboutWebViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    
+    print("Memory Warning")
+  }
+}
