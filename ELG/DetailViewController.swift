75a771818ce8b6004b9bcdbcf5110e08a0040d91
Johannes Jakob <johannes.jakob@elg-halle.de>
2016-12-05 21:08:40 +0100

    Add functionality to DetailViewController.swift

--- ELG/DetailViewController.swift
+++ ELG/DetailViewController.swift
@@ -9,9 +9,20 @@
 import UIKit
 
 class DetailViewController: UIViewController {
+  // Outlets
+  
+  @IBOutlet weak private var detailLabel: UILabel!
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    
+    // Set label's text based on iOS version
+    
+    if #available(iOS 8, *) {
+      detailLabel.text = "Tippe links auf einen Menüpunkt oder wähle in Einstellungen → Startseite auswählen eine Startseite aus, die nach dem Start der App hier angezeigt wird."
+    } else {
+      detailLabel.text = "Wische vom linken Bildschirmrand nach rechts, um das Menü anzuzeigen."
+    }
   }
   
   override func didReceiveMemoryWarning() {
