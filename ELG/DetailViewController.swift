fb2daa3298f9c24a98c939ca6980aee9ff602b59
Johannes Jakob <johannes.jakob@elg-halle.de>
2016-12-05 21:00:58 +0100

    Add DetailViewController.swift

---
+++ ELG/DetailViewController.swift
@@ -0,0 +1,22 @@
+//
+//  DetailViewController.swift
+//  ELG
+//
+//  Created by Johannes Jakob on 05/12/2016
+//  © 2016 Elisabeth-Gymnasium Halle, Johannes Jakob
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    
+    print("Memory Warning")
+  }
+}
