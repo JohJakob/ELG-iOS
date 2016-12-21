ea37790cc6b5fa4ccdd11533257cc9477e147d50
Johannes Jakob <johannes.jakob@elg-halle.de>
2016-12-21 21:40:56 +0100

    Change TodayViewController.swift to UITableViewController

--- OmissionsWidget/TodayViewController.swift
+++ OmissionsWidget/TodayViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import NotificationCenter
 
-class TodayViewController: UIViewController, NCWidgetProviding {
+class TodayViewController: UITableViewController, NCWidgetProviding {
   override func viewDidLoad() {
     super.viewDidLoad()
   }
