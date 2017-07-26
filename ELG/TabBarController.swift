b07816911df9e56913683483cfc17d8a2a437fa5
Johannes Jakob <johannes@bullg.it>
2017-07-26 08:26:54 +0200

    Add TabBarController.swift

---
+++ ELG/TabBarController.swift
@@ -0,0 +1,21 @@
+//
+//  TabBarController.swift
+//  ELG
+//
+//  Created by Johannes Jakob on 26/07/2017
+//  © 2017 Elisabeth-Gymnasium Halle, Johannes Jakob
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+	
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		
+		print("Memory Warning")
+	}
+}
