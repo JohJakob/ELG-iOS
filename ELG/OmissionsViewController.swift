6577a03c8eb5523ad554e2f236f695f2682619a7
Johannes Jakob <johannes@bullg.it>
2017-08-26 12:48:59 +0200

    Update grades array

--- ELG/OmissionsViewController.swift
+++ ELG/OmissionsViewController.swift
@@ -21,7 +21,7 @@ class OmissionsViewController: UITableViewController {
   var autoSave = Bool()
   var teacherMode = Bool()
   var teacherToken = String()
-  let grades = ["5a", "5b", "5c", "5d", "5e", "6a", "6b", "6c", "6d", "6e", "7a", "7b", "7c", "7d", "7e", "8a", "8b", "8c", "8d", "9a", "9b", "9c", "9d", "10a", "10b", "10c", "10d", "11a", "11b", "11c", "11d", "11e", "12a", "12b", "12c", "12d", "12e"]
+  let grades = ["Keine Klasse", "5a", "5b", "5c", "5d", "5e", "6a", "6b", "6c", "6d", "6e", "7a", "7b", "7c", "7d", "7e", "8a", "8b", "8c", "8d", "9a", "9b", "9c", "9d", "10a", "10b", "10c", "10d", "11a", "11b", "11c", "11d", "11e", "12a", "12b", "12c", "12d", "12e"]
   
   @IBAction func saveButtonTap(_ sender: UIBarButtonItem) {
     saveOmissions()
