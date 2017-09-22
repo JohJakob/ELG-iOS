cce640494ba5eae965c9367e3c00721095e1e2ce
Johannes Jakob <johannes@bullg.it>
2017-09-22 11:35:57 +0200

    Add NoConnectionView

---
+++ ELG/NoConnectionView.swift
@@ -0,0 +1,86 @@
+//
+//  NoConnectionView.swift
+//  ELG
+//
+//  Created by Johannes Jakob on 19/09/2017
+//  © 2017 Elisabeth-Gymnasium Halle, Johannes Jakob
+//
+
+import UIKit
+
+final class NoConnectionView: UIView {
+	// MARK: - Properties
+	
+	public var textLabel: UILabel = {
+		let view = UILabel()
+		
+		view.numberOfLines = 0
+		view.layoutMargins = UIEdgeInsetsMake(0, 16, 0, 16)
+		
+		view.translatesAutoresizingMaskIntoConstraints = false
+		
+		return view
+	}()
+	
+	// MARK: - Initializers
+	
+	convenience init() {
+		self.init(frame: CGRect())
+	}
+	
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+		
+		initialize()
+	}
+	
+	required init?(coder aDecoder: NSCoder) {
+		super.init(coder: aDecoder)
+		
+		initialize()
+	}
+	
+	// MARK: - UIView
+	
+	override func layoutSubviews() {
+		super.layoutSubviews()
+	}
+	
+	// MARK: - Public
+	
+	public func defaultText() -> NSMutableAttributedString {
+		let headingParagraphStyle = NSMutableParagraphStyle()
+		
+		headingParagraphStyle.alignment = .center
+		
+		let detailsParagraphStyle = NSMutableParagraphStyle()
+		
+		detailsParagraphStyle.alignment = .center
+		detailsParagraphStyle.lineHeightMultiple = 1.2
+		
+		let heading = NSMutableAttributedString(string: "Keine Verbindung\n", attributes: [NSFontAttributeName: UIFont.boldSystemFont(ofSize: 20), NSParagraphStyleAttributeName: headingParagraphStyle])
+		let details = NSMutableAttributedString(string: "Bitte überprüfe die Einstellungen. Tippe hier, um die Seite neu zu laden.", attributes: [NSFontAttributeName: UIFont.systemFont(ofSize: 16), NSParagraphStyleAttributeName: detailsParagraphStyle])
+		
+		let string = NSMutableAttributedString()
+		
+		string.append(heading)
+		string.append(details)
+		
+		string.addAttribute(NSForegroundColorAttributeName, value: UIColor.gray, range: NSRange(location: 0, length: string.length))
+		
+		return string
+	}
+	
+	// MARK: - Private
+	
+	private func initialize() {
+		addSubview(textLabel)
+		
+		NSLayoutConstraint.activate([
+			NSLayoutConstraint(item: textLabel, attribute: .leading, relatedBy: .equal, toItem: self, attribute: .leading, multiplier: 1, constant: 0),
+			NSLayoutConstraint(item: textLabel, attribute: .trailing, relatedBy: .equal, toItem: self, attribute: .trailing, multiplier: 1, constant: 0),
+			
+			NSLayoutConstraint(item: textLabel, attribute: .centerY, relatedBy: .equal, toItem: self, attribute: .centerY, multiplier: 1, constant: 0)
+		])
+	}
+}
