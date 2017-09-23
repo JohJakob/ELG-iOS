e16f0b4f7937edb0b0f1a9bcdcf8d6727df28ff3
Johannes Jakob <johannes@bullg.it>
2017-09-23 14:20:16 +0200

    Improve functionality

--- ELG/FoerdervereinViewController.swift
+++ ELG/FoerdervereinViewController.swift
@@ -8,17 +8,16 @@
 
 import UIKit
 
-class FoerdervereinViewController: UITableViewController {
+class FoerdervereinViewController: UITableViewController, UIGestureRecognizerDelegate, XMLParserDelegate {
 	// MARK: - Properties
 	
 	@IBOutlet var segmentedControl: UISegmentedControl!
   
   var defaults: UserDefaults!
-  var items = [[String: String]]()
-  var item = Dictionary<String, String>()
-  var itemTitle = String()
-  var itemDescription = String()
-  var itemLink = String()
+	var articles = [[String: String]]()
+	var currentArticle = [String: String]()
+	let weekdays = ["Mon": "Montag", "Tue": "Dienstag", "Wed": "Mittwoch", "Thu": "Donnerstag", "Fri": "Freitag", "Sat": "Samstag", "Sun": "Sonntag"]
+	let months = ["Jan": "Januar", "Feb": "Februar", "Mar": "März", "Apr": "April", "May": "Mai", "Jun": "Juni", "Jul": "Juli", "Aug": "August", "Sep": "September", "Oct": "Oktober", "Nov": "November", "Dec": "Dezember"]
   let articleViewController = UIStoryboard(name: "Main", bundle: Bundle.main).instantiateViewController(withIdentifier: "FoerdervereinArticleViewController")
 	
 	// MARK: - UITableViewController
@@ -26,39 +25,7 @@ class FoerdervereinViewController: UITableViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
 		
-		segmentedControl.addTarget(self, action: #selector(FoerdervereinViewController.changeView), for: .valueChanged)
-    
-    // Initialize user defaults
-		
-		defaults = UserDefaults.init(suiteName: "group.com.hardykrause.elg")
-		
-    // Set up refresh control
-    
-    let articlesRefreshControl = UIRefreshControl.init()
-    
-    articlesRefreshControl.addTarget(self, action: #selector(FoerdervereinViewController.refreshTableView), for: .valueChanged)
-    
-    refreshControl = articlesRefreshControl
-    
-    // Check internet reachability
-    
-    let reachabilityStatus: NetworkStatus = Reachability.forInternetConnection().currentReachabilityStatus()
-    
-    if reachabilityStatus != NotReachable {
-      initParser()
-    } else {
-      let noConnectionLabel = UILabel.init()
-      noConnectionLabel.text = "Keine Verbindung"
-      noConnectionLabel.textColor = UIColor.lightGray
-      noConnectionLabel.font = UIFont.boldSystemFont(ofSize: 18)
-      noConnectionLabel.textAlignment = .center
-      
-      // Change table view appearance
-      
-      tableView.backgroundColor = UIColor.groupTableViewBackground
-      tableView.separatorStyle = .none
-      tableView.backgroundView = noConnectionLabel
-    }
+		initialize()
   }
 	
 	override func didReceiveMemoryWarning() {
@@ -71,9 +38,7 @@ class FoerdervereinViewController: UITableViewController {
   
   override func numberOfSections(in tableView: UITableView) -> Int {
     var numberOfSections = Int()
-    
-    // Check internet reachability
-    
+		
     let reachabilityStatus: NetworkStatus = Reachability.forInternetConnection().currentReachabilityStatus()
     
     if reachabilityStatus != NotReachable {
@@ -86,39 +51,53 @@ class FoerdervereinViewController: UITableViewController {
   }
   
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return items.count
+    return articles.count
   }
   
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCell(withIdentifier: "FoerdervereinTableViewCell", for: indexPath)
-    
-    // Set table view cell's text
-    
-    cell.textLabel!.text = items[indexPath.row]["title"]
-    cell.detailTextLabel!.text = items[indexPath.row]["description"]
-    
-    return cell
+		let cell = tableView.dequeueReusableCell(withIdentifier: "SimpleFeedTableViewCell", for: indexPath) as? SimpleFeedTableViewCell
+		
+		cell?.headingLabel.text = articles[indexPath.row]["heading"]
+		cell?.detailsLabel.text = articles[indexPath.row]["date"]
+		
+		cell?.selectionStyle = .none
+		
+		return cell!
   }
-  
+	
+	override func tableView(_ tableView: UITableView, didHighlightRowAt indexPath: IndexPath) {
+		highlightCell(indexPath: indexPath)
+	}
+	
+	override func tableView(_ tableView: UITableView, didUnhighlightRowAt indexPath: IndexPath) {
+		unhighlightCell(indexPath: indexPath)
+	}
+	
   override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-    // Deselect table view cell
-    
-    tableView.deselectRow(at: indexPath, animated: true)
-    
-    // Save user defaults
-    
-    defaults.setValue(items[indexPath.row]["title"], forKey: "selectedArticleTitle")
-    defaults.setValue(items[indexPath.row]["link"], forKey: "selectedArticleLink")
+    defaults.setValue(articles[indexPath.row]["heading"], forKey: "selectedArticleTitle")
+    defaults.setValue(articles[indexPath.row]["link"], forKey: "selectedArticleLink")
     defaults.synchronize()
-    
-    // Show new view
 		
 		navigationController?.show(articleViewController, sender: self)
   }
-  
-  // MARK: - Custom
 	
-	func changeView() {
+	// MARK: - Private
+	
+	private func initialize() {
+		defaults = UserDefaults.init(suiteName: "group.com.hardykrause.elg")
+		
+		segmentedControl.addTarget(self, action: #selector(changeView), for: .valueChanged)
+		
+		initializeTableView()
+		
+		let articlesRefreshControl = UIRefreshControl.init()
+		
+		articlesRefreshControl.addTarget(self, action: #selector(showArticles), for: .valueChanged)
+		
+		refreshControl = articlesRefreshControl
+	}
+	
+	@objc private func changeView() {
 		var navigationStack = navigationController?.viewControllers
 		let newsViewController = UIStoryboard(name: "Main", bundle: Bundle.main).instantiateViewController(withIdentifier: "NewsViewController")
 		
@@ -126,82 +105,124 @@ class FoerdervereinViewController: UITableViewController {
 		navigationStack?.insert(newsViewController, at: (navigationStack?.count)!)
 		navigationController?.setViewControllers(navigationStack!, animated: false)
 	}
-  
-  func initParser() {
-    // Create task
-    
-		let session = URLSession.shared
-    var request = URLRequest(url: URL(string: "https://svelg.wordpress.com/feed")!)
-    var error: NSError?
-    
-    request.httpMethod = "GET"
-    
-    let task = URLSession.shared.dataTask(with: request, completionHandler: {
-      (data, response, error) in
-      
-      if data == nil {
-        // Error
-        
-        return
-      }
-      
-      // Parse feed
-      
-      func parse() {
-        // Create parser
-        
-        let xml = SWXMLHash.parse(data!)
-        
-        // Parse feed
-        
-        for feedElement in xml["rss"]["channel"]["item"].all {
-          self.item["title"] = feedElement["title"].element?.text
-          self.item["description"] = feedElement["description"].element?.text
-          self.item["link"] = feedElement["link"].element?.text
-          
-          self.items.append(self.item)
-        }
-        
-        // Reload table view
-        
-        self.tableView.reloadData()
-        
-        // Reset table view appearance
-        
-        self.tableView.backgroundColor = UIColor.white
-        self.tableView.separatorStyle = .singleLine
-      }
-      
-      // Run task asynchronously
-      
-      DispatchQueue.main.async(execute: parse)
-    }) 
-    
-    task.resume()
-  }
-  
-  func refreshTableView() {
-    let reachabilityStatus: NetworkStatus = Reachability.forInternetConnection().currentReachabilityStatus()
-    
-    if reachabilityStatus != NotReachable {
-      initParser()
-    } else {
-      // Reload table view
-      
-      tableView.reloadData()
-      
-      // Display label instead of table view
-      
-      let noConnectionLabel = UILabel.init()
-      noConnectionLabel.text = "Keine Internetverbindung"
-      noConnectionLabel.textColor = UIColor.lightGray
-      noConnectionLabel.font = UIFont.systemFont(ofSize: 16)
-      noConnectionLabel.textAlignment = .center
-      
-      tableView.separatorStyle = .none
-      tableView.backgroundView = noConnectionLabel
-    }
-    
-    refreshControl?.endRefreshing()
-  }
+	
+	private func initializeTableView() {
+		tableView.register(SimpleFeedTableViewCell.self, forCellReuseIdentifier: "SimpleFeedTableViewCell")
+		
+		tableView.estimatedRowHeight = 97
+		tableView.rowHeight = UITableViewAutomaticDimension
+		
+		tableView.backgroundColor = UIColor.groupTableViewBackground
+		tableView.separatorColor = UIColor.clear
+		tableView.separatorStyle = .none
+	}
+	
+	@objc private func showArticles() {
+		let reachabilityStatus: NetworkStatus = Reachability.forInternetConnection().currentReachabilityStatus()
+		
+		if reachabilityStatus != NotReachable {
+			download(completion: { (items) in
+				self.articles = items
+				self.tableView.reloadData()
+			})
+		} else {
+			let noConnectionView = NoConnectionView()
+			let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapRecognized(tapRecognizer:)))
+			
+			noConnectionView.textLabel.attributedText = noConnectionView.defaultText()
+			noConnectionView.addGestureRecognizer(tapRecognizer)
+			
+			tableView.backgroundColor = UIColor.groupTableViewBackground
+			tableView.separatorStyle = .none
+			tableView.backgroundView = noConnectionView
+		}
+		
+		refreshControl?.endRefreshing()
+	}
+	
+	private func download(completion: @escaping (_ articles: [[String: String]]) -> ()) {
+		let reachabilityStatus: NetworkStatus = Reachability.forInternetConnection().currentReachabilityStatus()
+		
+		if reachabilityStatus != NotReachable {
+			var request = URLRequest(url: URL(string: "https://svelg.wordpress.com/feed")!)
+			
+			request.httpMethod = "GET"
+			
+			URLSession.shared.dataTask(with: request, completionHandler: { (data, response, error) in
+				DispatchQueue.main.async {
+					if let error = error {
+						let errorAlertController = UIAlertController(title: "Fehler", message: "Es ist folgender Fehler aufgetreten: \(error.localizedDescription)", preferredStyle: .alert)
+						
+						errorAlertController.addAction(UIAlertAction(title: "OK", style: .cancel, handler: { _ in
+							self.dismiss(animated: true, completion: nil)
+						}))
+						
+						self.present(errorAlertController, animated: true, completion: nil)
+						
+						return
+					}
+					
+					guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+						let responseErrorAlertController = UIAlertController(title: "Server-Fehler", message: "Die Artikel konnten nicht geladen werden.", preferredStyle: .alert)
+						
+						responseErrorAlertController.addAction(UIAlertAction(title: "OK", style: .cancel, handler: { _ in
+							self.dismiss(animated: true, completion: nil)
+						}))
+						
+						self.present(responseErrorAlertController, animated: true, completion: nil)
+						
+						return
+					}
+					
+					let xml = SWXMLHash.parse(data!)
+					var items = [[String: String]]()
+					
+					for element in xml["rss"]["channel"]["item"].all {
+						var currentItem = [String: String]()
+						
+						currentItem["heading"] = element["title"].element?.text
+						currentItem["date"] = self.transformDate(string: (element["pubDate"].element?.text)!)
+						currentItem["link"] = element["link"].element?.text
+						
+						items.append(currentItem)
+					}
+					
+					completion(items)
+				}
+			}).resume()
+		}
+	}
+	
+	private func transformDate(string: String) -> String {
+		let dateFormatter = DateFormatter()
+		
+		dateFormatter.dateFormat = "EEE, dd MMM yyyy HH:mm:ss +zzzz"
+		
+		let date = dateFormatter.date(from: string)
+		
+		dateFormatter.dateFormat = "EEEE, dd. MMMM yyyy"
+		dateFormatter.locale = Locale.init(identifier: "de_DE")
+		
+		return dateFormatter.string(from: date!).uppercased()
+	}
+	
+	@objc private func tapRecognized(tapRecognizer: UITapGestureRecognizer) {
+		showArticles()
+	}
+	
+	private func highlightCell(indexPath: IndexPath) {
+		let cell = tableView.cellForRow(at: indexPath) as! SimpleFeedTableViewCell
+		
+		UIView.animate(withDuration: 0.2, animations: {
+			cell.containerView.transform = CGAffineTransform(scaleX: 0.95, y: 0.95)
+		})
+	}
+	
+	private func unhighlightCell(indexPath: IndexPath) {
+		let cell = tableView.cellForRow(at: indexPath) as! SimpleFeedTableViewCell
+		
+		UIView.animate(withDuration: 0.2, animations: {
+			cell.containerView.transform = CGAffineTransform(scaleX: 1, y: 1)
+		})
+	}
 }
