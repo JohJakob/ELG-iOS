b4d85eb2c8d4e6389b260e66e3abca780a5fcea9
Johannes Jakob <hi@johjakob.com>
2020-08-02 16:18:21 +0200

    fix: Fix release notes presentation on first launch

--- ELG/TabBarController.swift
+++ ELG/TabBarController.swift
@@ -13,7 +13,7 @@ class TabBarController: UITabBarController, SFSafariViewControllerDelegate {
 	// MARK: - Properties
 	
 	var defaults: UserDefaults!
-	var onboardingViewController = UIViewController()
+	var onboardingNavigationController = UINavigationController()
 	let version = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String
 	let grades = ["Keine Klasse", "5a", "5b", "5c", "5d", "5e", "6a", "6b", "6c", "6d", "6e", "7a", "7b", "7c", "7d", "7e", "8a", "8b", "8c", "8d", "8e", "9a", "9b", "9c", "9d", "9e", "10a", "10b", "10c", "10d", "10e", "11a", "11b", "11c", "11d", "11e", "12a", "12b", "12c", "12d", "12e"]
 	
@@ -22,12 +22,7 @@ class TabBarController: UITabBarController, SFSafariViewControllerDelegate {
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
-		if #available(iOS 11, *) {
-			onboardingViewController = UIStoryboard(name: "Main", bundle: Bundle.main).instantiateViewController(withIdentifier: "OnboardingNavigationController")
-		} else {
-			onboardingViewController = UIStoryboard(name: "MainLegacy", bundle: Bundle.main).instantiateViewController(withIdentifier: "OnboardingNavigationController")
-		}
-		
+		// Initialize user defaults
 		defaults = UserDefaults.init(suiteName: "group.com.johjakob.elg")
 		
 		setUp()
@@ -36,7 +31,20 @@ class TabBarController: UITabBarController, SFSafariViewControllerDelegate {
 	override func viewDidAppear(_ animated: Bool) {
 		super.viewDidAppear(animated)
 		
-		defaults.removeObject(forKey: "launched3.0")
+		// Create key name for current version
+		let currentVersionKey = "launched" + version!
+		
+		// Check if this is the current version’s first launch
+		// Present release notes on first launch
+		if defaults.object(forKey: currentVersionKey) == nil {
+			if #available(iOS 11, *) {
+				onboardingNavigationController = UIStoryboard(name: "Main", bundle: Bundle.main).instantiateViewController(withIdentifier: "OnboardingNavigationController") as! UINavigationController
+			} else {
+				onboardingNavigationController = UIStoryboard(name: "MainLegacy", bundle: Bundle.main).instantiateViewController(withIdentifier: "OnboardingNavigationController") as! UINavigationController
+			}
+			
+			present(onboardingNavigationController, animated: true, completion: nil)
+		}
 	}
 	
 	override func didReceiveMemoryWarning() {
