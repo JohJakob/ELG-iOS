5afb6381718c63ec0c150df0306c6fbae36a9ff8
Johannes Jakob <hi@johjakob.com>
2023-03-16 15:17:25 +0100

    Project: Add new app target

---
+++ ELG/ELGApp.swift
@@ -0,0 +1,18 @@
+//
+//  ELGApp.swift
+//  ELG
+//
+//  Created by Johannes Jakob on 16.03.23.
+//  Copyright © 2023 Elisabeth-Gymnasium Halle. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct ELGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
